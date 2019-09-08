87c957e844de47ab61c02cce551b8896e3ee2dcc
kasperskov01 <kasperskov01@gmail.com>
2019-09-08 19:45:46 +0200

    Updating the contact form.

--- src/components/sections/Kontakt.js
+++ src/components/sections/Kontakt.js
@@ -25,39 +25,32 @@ const Kontakt = () => (
       <Container>
         {data.allSanityGeneral.edges.map(({ node }) => (
           <div key="container">
-              Kontakt
-              <br />
-              Her kan du sende en besked til mig
-              <br />
-              <br />
-              <br />
-              <form name="contact" method="POST" data-netlify="true">
-                <p>
-                  <label>
-                    Dit navn: <input type="text" name="name" />
-                  </label>
-                </p>
-                <p>
-                  <label>
-                    Din email: <input type="email" name="email" />
-                  </label>
-                </p>
-                <p>
-                  <label>
-                    Besked: <textarea name="message"></textarea>
-                  </label>
-                </p>
-                <p hidden>
-                  <label>
-                    Don’t fill this out if you're human:{' '}
-                    <input name="bot-field" />
-                  </label>
-                </p>
-                <p>
-                  <button type="submit">Send</button>
-                </p>
-              </form>
-              {node.mobile}
+            Kontakt
+            <br />
+            Her kan du sende en besked til mig
+            <br />
+            <br />
+            <br />
+            <form name="contact" method="POST" data-netlify="true">
+              <label>
+                Dit navn: <input type="text" name="name" />
+              </label>
+              <label>
+                Din email: <input type="email" name="email" />
+              </label>
+
+              <label>
+                Besked: <textarea name="message"></textarea>
+              </label>
+
+              <label hidden="true">
+                Don’t fill this out if you're human: <input name="bot-field" />
+              </label>
+              <p>
+                <button type="submit">Send</button>
+              </p>
+            </form>
+            {node.mobile}
           </div>
         ))}
       </Container>
