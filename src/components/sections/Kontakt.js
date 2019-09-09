13c1175eaa3badac8f8424c09d2f33d9ca4a8495
kasperskov01 <kasperskov01@gmail.com>
2019-09-09 17:07:49 +0200

    Form.

--- src/components/sections/Kontakt.js
+++ src/components/sections/Kontakt.js
@@ -25,32 +25,20 @@ const Kontakt = () => (
       <Container>
         {data.allSanityGeneral.edges.map(({ node }) => (
           <div key="container">
-            Kontakt
-            <br />
-            Her kan du sende en besked til mig
-            <br />
-            <br />
-            <br />
-            <form name="contact" method="POST" data-netlify="true">
-              <label>
-                Dit navn: <input type="text" name="name" />
-              </label>
-              <label>
-                Din email: <input type="email" name="email" />
-              </label>
+            <h1>Contact</h1>
 
-              <label>
-                Besked: <textarea name="message"></textarea>
-              </label>
-
-              <label hidden="true">
-                Don’t fill this out if you're human: <input name="bot-field" />
-              </label>
-              <p>
-                <button type="submit">Send</button>
-              </p>
+            <form name="Contact Form" method="POST" data-netlify="true">
+              <input type="hidden" name="form-name" value="Contact Form" />
+              <div>
+                <label>Your Email:</label>
+                <input type="email" name="email" />
+              </div>
+              <div>
+                <label>Message:</label>
+                <textarea name="message" />
+              </div>
+              <button type="submit">Send</button>
             </form>
-            {node.mobile}
           </div>
         ))}
       </Container>
