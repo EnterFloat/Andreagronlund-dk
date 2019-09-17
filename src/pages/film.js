1242a394a3ad918600ff178e45e5ea0f03b3b9c6
kasperskov01 <kasperskov01@gmail.com>
2019-09-17 21:20:35 +0200

    Debugging build. Editing pages.

--- src/pages/film.js
+++ src/pages/film.js
@@ -1,23 +1,19 @@
 import React from 'react';
 import Layout from '@common/Layout';
 import MainNavbar from '@common/Navbar';
+import Film from '@sections/Film';
 import Footer from '@sections/Footer';
 import Video from '@common/Video';
 
 // import { Button } from 'react-bootstrap';
 
-const Film = () => (
+const FilmPage = () => (
   
   <Layout>
     <MainNavbar />
-    <h1>Film</h1>
-    {/* <Button variant="primary">primary</Button> */}
-    <Video
-        videoSrcURL="https://www.youtube.com/embed/dQw4w9WgXcQ"
-        videoTitle="Official Music Video on YouTube"
-      />
+    <Film />
     <Footer /> 
   </Layout>
 );
 
-export default Film
+export default FilmPage
