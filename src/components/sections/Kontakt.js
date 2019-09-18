ec05f6bcee430e0769854fdbb7933a2be5f09396
kasperskov01 <kasperskov01@gmail.com>
2019-09-18 20:21:28 +0200

    Better textarea in contact form.

--- src/components/sections/Kontakt.js
+++ src/components/sections/Kontakt.js
@@ -30,24 +30,34 @@ const Kontakt = () => (
             <h2>Kontakt</h2>
             <br></br>
             <Form name="Contact Form" method="POST" data-netlify="true">
+              <Form.Group controlId="formBasicText">
+                <Form.Label>Navn</Form.Label>
+                <Form.Control
+                  name="name"
+                  type="text"
+                  placeholder="Hvad hedder du?"
+                />
+              </Form.Group>
+
               <Form.Group controlId="formBasicEmail">
                 <Form.Label>E-mailadresse</Form.Label>
-                <Form.Control type="email" placeholder="Indtast din email" />
+                <Form.Control
+                  name="email"
+                  type="email"
+                  placeholder="Indtast din email"
+                />
                 <Form.Text className="text-muted">
                   Vi deler aldrig din email med nogen andre.
                 </Form.Text>
               </Form.Group>
 
-              <Form.Group controlId="formBasicText">
-                <Form.Label>Emne</Form.Label>
-                <Form.Control type="text" placeholder="Hvad handler det om?" />
-              </Form.Group>
-
-              <Form.Group controlId="formBasicTextarea">
+              <Form.Group controlId="exampleForm.ControlTextarea1">
                 <Form.Label>Besked</Form.Label>
                 <Form.Control
-                  type="textarea"
+                  name="message"
                   placeholder="Skriv din besked her"
+                  as="textarea"
+                  rows="3"
                 />
               </Form.Group>
 
