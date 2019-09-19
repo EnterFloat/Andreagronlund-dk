57b64b237662e58b072bc139c332804874de9d66
kasperskov01 <kasperskov01@gmail.com>
2019-09-19 20:34:14 +0200

    Finished Om mig.

--- src/components/sections/OmMig.js
+++ src/components/sections/OmMig.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import { StaticQuery, graphql } from 'gatsby';
+// import styled from 'styled-components';
 import { Container } from '@components/global';
+import Img from 'gatsby-image';
+import { Row, Col, Container as BootstrapContainer } from 'react-bootstrap';
 
 const OmMig = () => (
   <StaticQuery
@@ -16,25 +19,119 @@ const OmMig = () => (
               mobile
               instagram
               dask
+              aboutme
+              birthdate
+              position
+              height
+              weight
+              haircolor
+              eyecolor
+              location
+              id
+              aboutmeimage {
+                asset {
+                  fluid(maxWidth: 872, maxHeight: 981) {
+                    ...GatsbySanityImageFluid
+                  }
+                }
+              }
             }
           }
         }
       }
     `}
     render={data => (
-      <Container>
+      <BootstrapContainer key="container">
         {data.allSanityGeneral.edges.map(({ node }) => (
-          <div key="container">
-            <p>
-              Om mig
+          <>
+            <Row>
               <br />
-              {node.mobile}
-            </p>
-          </div>
+            </Row>
+            <h2>Om mig</h2>
+            <br />
+            <Row>
+              <Col
+                xs={{ span: 8, offset: 2 }}
+                sm={{ span: 8, offset: 2 }}
+                md={{ span: 6, offset: 3 }}
+                lg={{ span: 4, offset: 0 }}
+                xl={{ span: 4, offset: 0 }}
+                style={{ marginBottom: 30 }}
+              >
+                <Img key={'Image'} fluid={node.aboutmeimage.asset.fluid} />
+              </Col>
+              <Col
+                xs={{ span: 12, offset: 0 }}
+                sm={{ span: 12, offset: 0 }}
+                md={{ span: 10, offset: 1 }}
+                lg={{ span: 8, offset: 0 }}
+                xl={{ span: 8, offset: 0 }}
+              >
+                <p style={{ whiteSpace: 'pre-wrap' }}>{node.aboutme}</p>
+                <br />
+                <br />
+              </Col>
+            </Row>
+            <Row>
+              <Col
+                style={{ marginTop: 20 }}
+                md={{ span: 10, offset: 1 }}
+                sm={{ span: 12, offset: 0 }}
+                xs={{ span: 12, offset: 0 }}
+              >
+                <Attribute
+                  category={'Alder'}
+                  value={<Age birthdate={node.birthdate} />}
+                  unit={'år'}
+                />
+                <Attribute
+                  category={'Øjenfarve'}
+                  value={node.eyecolor}
+                  unit={''}
+                />
+                <Attribute
+                  category={'Hårfarve'}
+                  value={node.haircolor}
+                  unit={''}
+                />
+                <Attribute category={'Højde'} value={node.height} unit={'cm'} />
+                <Attribute category={'Vægt'} value={node.weight} unit={'kg'} />
+                <Attribute
+                  category={'Stilling'}
+                  value={node.position}
+                  unit={''}
+                />
+                <Attribute
+                  category={'Placering'}
+                  value={node.location}
+                  unit={''}
+                />
+              </Col>
+            </Row>
+          </>
         ))}
-      </Container>
+      </BootstrapContainer>
     )}
   />
 );
+
+function Attribute(props) {
+  return (
+    <p style={{ marginBottom: 14 }}>
+      <span style={{ color: '#8f8f8f', marginRight: 10 }}>
+        {props.category}
+        {': '}
+      </span>
+      {props.value} {props.unit}
+    </p>
+  );
+}
+function Age(props) {
+  console.log(props.birthdate);
+  var start = new Date(props.birthdate);
+  var today = new Date();
+  var difference = new Date(today - start).getFullYear() - 1970;
+  return difference;
+}
 
 export default OmMig;
