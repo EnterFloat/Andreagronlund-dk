001c27b27888b7ef0a450a1185c3772c6397dfc6
kasperskov01 <kasperskov01@gmail.com>
2019-09-20 21:06:51 +0200

    Working CV.

--- src/components/sections/Cv.js
+++ src/components/sections/Cv.js
@@ -6,9 +6,261 @@ import {
   Col,
   Container as BootstrapContainer,
   Table,
+  ButtonToolbar,
+  ToggleButtonGroup,
+  ToggleButton,
 } from 'react-bootstrap';
 
-const Cv = () => (
+import { Component } from 'react';
+
+class Cv extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { selectedOption: '2' };
+  }
+  handleOptionChange = changeEvent => {
+    this.setState({
+      selectedOption: changeEvent.target.value,
+    });
+  };
+
+  SchemaHead(props) {
+    switch (props.option) {
+      case '1':
+        return (
+          <thead>
+            <tr>
+              <th>Titel</th>
+              <th>Rolle</th>
+              <th>Instruktør</th>
+              <th>Producent</th>
+            </tr>
+          </thead>
+        );
+        break;
+      case '2':
+        return (
+          <thead>
+            <tr>
+              <th>Titel</th>
+              <th>Rolle</th>
+              <th>Instruktør</th>
+              <th>Arrangør</th>
+            </tr>
+          </thead>
+        );
+        break;
+      case '3':
+        return (
+          <thead>
+            <tr>
+              <th>Titel</th>
+              <th>Underviser</th>
+              <th>Beskrivelse</th>
+            </tr>
+          </thead>
+        );
+        break;
+      case '4':
+        return (
+          <thead>
+            <tr>
+              <th>Titel</th>
+              <th>Beskrivelse</th>
+            </tr>
+          </thead>
+        );
+        break;
+      default:
+        return (
+          <thead>
+            <tr>
+              <th></th>
+              <th></th>
+              <th></th>
+              <th></th>
+            </tr>
+          </thead>
+        );
+    }
+  }
+
+  SchemaBody(props) {
+    switch (props.option) {
+      case '1':
+        return (
+          <tbody>
+            {props.data.allSanityFilmandtv.edges.map(({ node }) => (
+              <tr>
+                <td>
+                  {node.title}
+                  {' ('}
+                  {node.year}
+                  {')'}
+                </td>
+                <td>{node.role}</td>
+                <td>{node.instructor}</td>
+                <td>{node.producer}</td>
+              </tr>
+            ))}
+          </tbody>
+        );
+        break;
+      case '2':
+        return (
+          <tbody>
+            {props.data.allSanityTheater.edges.map(({ node }) => (
+              <tr>
+                <td>
+                  {node.title}
+                  {' ('}
+                  {node.year}
+                  {')'}
+                </td>
+                <td>{node.role}</td>
+                <td>{node.instructor}</td>
+                <td>{node.organiser}</td>
+              </tr>
+            ))}
+          </tbody>
+        );
+        break;
+      case '3':
+        return (
+          <tbody>
+            {props.data.allSanityEducation.edges.map(({ node }) => (
+              <tr>
+                <td>
+                  {node.title}
+                  {' ('}
+                  {node.year}
+                  {')'}
+                </td>
+                <td>{node.teacher}</td>
+                <td>{node.description}</td>
+              </tr>
+            ))}
+          </tbody>
+        );
+        break;
+      case '4':
+        return (
+          <tbody>
+            {props.data.allSanityOther.edges.map(({ node }) => (
+              <tr>
+                <td>{node.title}</td>
+                <td>{node.description}</td>
+              </tr>
+            ))}
+          </tbody>
+        );
+        break;
+      default:
+        return (
+          <tbody>
+            <tr>
+              <td />
+              <td />
+              <td />
+              <td />
+            </tr>
+          </tbody>
+        );
+    }
+  }
+
+  render() {
+    const { data } = this.props;
+    return (
+      <BootstrapContainer key="container">
+        {data.allSanityGeneral.edges.map(({ node }) => (
+          <>
+            <Row>
+              <Col>
+                <br />
+                <h2>CV</h2>
+                <br />
+              </Col>
+            </Row>
+            <Row>
+              <Col
+                xs={{ span: 8, offset: 2 }}
+                sm={{ span: 8, offset: 2 }}
+                md={{ span: 6, offset: 3 }}
+                lg={{ span: 4, offset: 0 }}
+                xl={{ span: 4, offset: 0 }}
+                style={{ marginBottom: 30 }}
+              >
+                <Img key={'Image'} fluid={node.cvimage.asset.fluid} />
+              </Col>
+              <Col
+                xs={{ span: 12, offset: 0 }}
+                sm={{ span: 12, offset: 0 }}
+                md={{ span: 12, offset: 0 }}
+                lg={{ span: 12, offset: 0 }}
+                xl={{ span: 12, offset: 0 }}
+              >
+                <ButtonToolbar>
+                  <ToggleButtonGroup
+                    type="radio"
+                    name="options"
+                    defaultValue={this.state.selectedOption}
+                  >
+                    <ToggleButton
+                      value={'1'}
+                      centered
+                      variant="outline-secondary"
+                      checked={this.state.selectedOption === '1'}
+                      onChange={this.handleOptionChange}
+                    >
+                      Film og TV
+                    </ToggleButton>
+                    <ToggleButton
+                      value={'2'}
+                      centered
+                      variant="outline-secondary"
+                      checked={this.state.selectedOption === '2'}
+                      onChange={this.handleOptionChange}
+                    >
+                      Teater
+                    </ToggleButton>
+                    <ToggleButton
+                      value={'3'}
+                      centered
+                      variant="outline-secondary"
+                      checked={this.state.selectedOption === '3'}
+                      onChange={this.handleOptionChange}
+                    >
+                      Kurser og uddannelser
+                    </ToggleButton>
+                    <ToggleButton
+                      value={'4'}
+                      centered
+                      variant="outline-secondary"
+                      checked={this.state.selectedOption === '4'}
+                      onChange={this.handleOptionChange}
+                    >
+                      Anden bedrift
+                    </ToggleButton>
+                  </ToggleButtonGroup>
+                </ButtonToolbar>
+                <br />
+                <Table striped bordered hover>
+                  <this.SchemaHead option={this.state.selectedOption} />
+                  <this.SchemaBody
+                    data={data}
+                    option={this.state.selectedOption}
+                  />
+                </Table>
+              </Col>
+            </Row>
+          </>
+        ))}
+      </BootstrapContainer>
+    );
+  }
+}
+export default props => (
   <StaticQuery
     query={graphql`
       query CvQuery {
@@ -67,52 +319,6 @@ const Cv = () => (
         }
       }
     `}
-    render={data => (
-      <BootstrapContainer key="container">
-        {data.allSanityGeneral.edges.map(({ node }) => (
-          <>
-            <Row>
-              <Col>
-                <br />
-                <h2>CV</h2>
-                <br />
-              </Col>
-            </Row>
-            <Row>
-              <Col
-                xs={{ span: 8, offset: 2 }}
-                sm={{ span: 8, offset: 2 }}
-                md={{ span: 6, offset: 3 }}
-                lg={{ span: 4, offset: 0 }}
-                xl={{ span: 4, offset: 0 }}
-                style={{ marginBottom: 30 }}
-              >
-                <Img key={'Image'} fluid={node.cvimage.asset.fluid} />
-              </Col>
-              <Col>
-                <Table striped bordered hover>
-                  <thead>
-                    <tr>
-                      <th>Titel</th>
-                      <th>Rolle</th>
-                    </tr>
-                  </thead>
-                  <tbody>
-                    {data.allSanityFilmandtv.edges.map(({ node }) => (
-                      <tr>
-                        <td>{node.title}{" - ("}{node.year}{")"}</td>
-                        <td>{node.role}</td>
-                      </tr>
-                    ))}
-                  </tbody>
-                </Table>
-              </Col>
-            </Row>
-          </>
-        ))}
-      </BootstrapContainer>
-    )}
+    render={data => <Cv data={data} {...props} />}
   />
 );
-
-export default Cv;
--- src/components/sections/Film.js
+++ src/components/sections/Film.js
@@ -1,12 +1,76 @@
 import { graphql, StaticQuery } from 'gatsby';
-import React from 'react';
+import React, { Component } from 'react';
 import { Container, Button } from 'react-bootstrap';
 import Video from '@common/Video';
 
-const Film = () => (
+// const Film = () => (
+//   <StaticQuery
+//     query={graphql`
+//       query FilmQuery {
+//         allSanityGeneral(limit: 1) {
+//           edges {
+//             node {
+//               name
+//             }
+//           }
+//         }
+//       }
+//     `}
+//     render={data => (
+//       <Container>
+//         {data.allSanityGeneral.edges.map(({ node }) => (
+//           <>
+//             <h3>Film</h3>
+//             <Button variant="primary">{node.name}</Button>
+//             <Video
+//               videoSrcURL="https://www.youtube.com/embed/dQw4w9WgXcQ"
+//               videoTitle="Official Music Video on YouTube"
+//             />
+//           </>
+//         ))}
+//       </Container>
+//     )}
+//   />
+// );
+
+// export default Film;
+
+class Film extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { selectedOption: 'option1' };
+  }
+  render() {
+    // console.log(this.props.projects)
+    // const general = this.props.allSanityGeneral.edges
+    // console.log(projects)
+    // const { data } = this.props; // add more props here as needed
+    console.log(this.props.data.site.buildTime);
+
+    // console.log(articles)
+    return (
+      <Container>
+        {this.props.data.allSanityGeneral.edges.map(({ node }) => (
+          <>
+            <h3>Film</h3>
+            <Button variant="primary">{node.name}</Button>
+            <Video
+              videoSrcURL="https://www.youtube.com/embed/dQw4w9WgXcQ"
+              videoTitle="Official Music Video on YouTube"
+            />
+          </>
+        ))}
+      </Container>
+    );
+  }
+}
+export default props => (
   <StaticQuery
     query={graphql`
       query FilmQuery {
+        site {
+          buildTime
+        }
         allSanityGeneral(limit: 1) {
           edges {
             node {
@@ -16,17 +80,6 @@ const Film = () => (
         }
       }
     `}
-    render={data => (
-      <Container>
-        <h3>Film</h3>
-        <Button variant="primary">primary</Button>
-        <Video
-          videoSrcURL="https://www.youtube.com/embed/dQw4w9WgXcQ"
-          videoTitle="Official Music Video on YouTube"
-        />
-      </Container>
-    )}
+    render={data => <Film data={data} {...props} />}
   />
 );
-
-export default Film;
