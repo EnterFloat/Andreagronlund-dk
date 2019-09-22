995a694e74dc921c3d2d75a1a7fe52871531fbf6
kasperskov01 <kasperskov01@gmail.com>
2019-09-22 20:13:17 +0200

    Fixed showreel and images.

--- src/components/sections/Castingbilleder.js
+++ src/components/sections/Castingbilleder.js
@@ -1,37 +1,37 @@
 import { graphql, StaticQuery } from 'gatsby';
 import React from 'react';
-import { Container, Card, Row, Col } from 'react-bootstrap';
+import { Container, Row, Col, Card } from 'react-bootstrap';
 import Img from 'gatsby-image';
+import { Component } from 'react';
 
-const Castingbilleder = () => (
-  <StaticQuery
-    query={graphql`
-      query CastingbillederQuery {
-        allSanityOtherimage {
-          edges {
-            node {
-              otherimage {
-                caption
-                asset {
-                  fluid(maxWidth: 872, maxHeight: 981) {
-                    ...GatsbySanityImageFluid
-                  }
-                }
-              }
-            }
-          }
-        }
-      }
-    `}
-    render={data => (
+
+class Castingbilleder extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { activeNavItem: '0' };
+  }
+  handleOptionChange = changeEvent => {
+    this.setState({
+      activeNavItem: changeEvent.target.value,
+    });
+  };
+
+  render() {
+    const { data } = this.props;
+
+    // data.allSanityOtherimage.edges.sort(function(a, b) {
+    //   return b.node.year - a.node.year;
+    // });
+
+    return (
       <Container>
         <br />
         <h3>Castingbilleder</h3>
         <Row>
           {data.allSanityOtherimage.edges.map(({ node }) => (
             <Col
-              xs={{ span: 10, offset: 1 }}
-              sm={{ span: 10, offset: 1 }}
+              xs={{ span: 12, offset: 0 }}
+              sm={{ span: 12, offset: 0 }}
               md={{ span: 6, offset: 0 }}
               lg={{ span: 4, offset: 0 }}
               xl={{ span: 4, offset: 0 }}
@@ -46,8 +46,29 @@ const Castingbilleder = () => (
           ))}
         </Row>
       </Container>
-    )}
+    );
+  }
+}
+export default props => (
+  <StaticQuery
+    query={graphql`
+      query CastingbillederQuery {
+          allSanityOtherimage {
+            edges {
+              node {
+                otherimage {
+                  caption
+                  asset {
+                    fluid(maxWidth: 872, maxHeight: 981) {
+                      ...GatsbySanityImageFluid
+                    }
+                  }
+                }
+              }
+            }
+          }
+        }
+    `}
+    render={data => <Castingbilleder data={data} {...props} />}
   />
 );
-
-export default Castingbilleder;
--- src/components/sections/Showreel.js
+++ src/components/sections/Showreel.js
@@ -2,11 +2,62 @@ import { graphql, StaticQuery } from 'gatsby';
 import React from 'react';
 import { Container, Row, Col, Card } from 'react-bootstrap';
 import Video from '@common/Video';
+import styled from 'styled-components';
+import { Component } from 'react';
 
-const Showreel = () => (
+
+class Showreel extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { activeNavItem: '0' };
+  }
+  handleOptionChange = changeEvent => {
+    this.setState({
+      activeNavItem: changeEvent.target.value,
+    });
+  };
+
+  render() {
+    const { data } = this.props;
+
+    const IframeContainer = styled.div`
+      position: relative;
+      width: 100%;
+      padding-bottom: 56.25%;
+      height: 0;
+    `;
+    return (
+      <Container>
+        <br />
+        <h3>Film</h3>
+        <Row>
+          {data.allSanityShowreel.edges.map(({ node }) => (
+            <Col
+              xs={{ span: 12, offset: 0 }}
+              sm={{ span: 12, offset: 0 }}
+              md={{ span: 12, offset: 0 }}
+              lg={{ span: 12, offset: 0 }}
+              xl={{ span: 12, offset: 0 }}
+              style={{ textAlign: 'center', marginTop: '30px' }}
+            >
+              <Card>
+              <Card.Body>
+                  <IframeContainer>
+                    <Video videoSrcURL={node.showreel} />
+                  </IframeContainer>
+                </Card.Body>
+              </Card>
+            </Col>
+          ))}
+        </Row>
+      </Container>
+    );
+  }
+}
+export default props => (
   <StaticQuery
     query={graphql`
-      query ShowreelQuery {
+    query ShowreelQuery {
         allSanityShowreel {
           edges {
             node {
@@ -16,24 +67,6 @@ const Showreel = () => (
         }
       }
     `}
-    render={data => (
-      <Container>
-        <br />
-        <h3>Showreel</h3>
-        <Row>
-          {data.allSanityShowreel.edges.map(({ node }) => (
-            <Col style={{ textAlign: 'center', marginTop: '30px' }}>
-              <Card>
-                <Card.Body>
-                  <Video videoSrcURL={node.showreel} width={'426'} height="240" />
-                </Card.Body>
-              </Card>
-            </Col>
-          ))}
-        </Row>
-      </Container>
-    )}
+    render={data => <Showreel data={data} {...props} />}
   />
 );
-
-export default Showreel;
